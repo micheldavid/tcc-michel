8bb87cac028bc2d616ac39ed0292b4615c3015cd
michel.david <michel.david@46133bc6-0c54-0410-abf5-d9d914c9954a>
2009-05-30 15:12:52 +0000

    permite executar dag a partir do caminho relativo

--- trunk/AppMan/appman-project/exehda/appman-bin/run-appman.sh
+++ trunk/AppMan/appman-project/exehda/appman-bin/run-appman.sh
@@ -1,7 +1,8 @@
-EXEHDA_HOME=/home/aluno/Desktop/TCC/AppMan/appman-project/exehda
+EXEHDA_HOME=/home/aluno/AppMan/appman-project/exehda
 APPMAN_BIN=$EXEHDA_HOME/appman-bin
 
-DAG_FILE=$APPMAN_BIN/dag/t10.dag
+DAG_FILE=`pwd`/$1
+#DAG_FILE=$APPMAN_BIN/dag/t10.dag
 #DAG_FILE=$APPMAN_BIN/dag/timer.dag
 
 COMMAND="$EXEHDA_HOME/bin/isam-run $APPMAN_BIN/appman-console.isam -- $DAG_FILE"
