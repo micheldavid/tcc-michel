ba159934bc3cdfb407c84ef842dbc1a55169e581
michel.david <michel.david@46133bc6-0c54-0410-abf5-d9d914c9954a>
2009-06-01 07:28:23 +0000

    usado na avaliação

--- trunk/AppMan/appman-project/src/exehda/resources/run-times.sh
+++ trunk/AppMan/appman-project/src/exehda/resources/run-times.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 times=$1
 task=$2
-i=0
-
-while [ $times -ge $i ]
+i=1
 
 APPMAN_BIN=$EXEHDA_HOME/appman-bin
+
+rm -r $EXEHDA_HOME/log/*
+
+while [ $i -le $times ]
+do
+
+DTSTART=`date --rfc-3339='ns'`
+echo $DTSTART>>$EXEHDA_HOME/log/dtstart.txt
+echo "${DTSTART}: executando tarefa nro $i";
 $APPMAN_BIN/run-appman.sh $task
+
+#verificando finalizacao da tarefa
 while [ 1 ]
 do
-    cat /proc/loadavg >> $EXEHDA_HOME/resultados/load$i;
+#    cat /proc/loadavg >> $EXEHDA_HOME/resultados/$i;
     sleep 1s;
     if [ -f "$EXEHDA_HOME/log/tempoExecucao.txt" ]
     then
-        nlines=` wc -l ./exehda/bin/parseOut.txt | gawk '{print $1}'`
+        nlines=` wc -l $EXEHDA_HOME/log/tempoExecucao.txt | gawk '{print $1}'`
     else
         nlines=0
     fi
-#    if [ $nlines -ge 4 ]
-#    then 
-        echo parseOut com 4 linhas...
+    if [ $nlines -ge 3 ]
+    then 
+        echo tempoExecucao.txt com 3 linhas...
         break;
-#    fi
+    fi
 done
 
 mkdir -p $EXEHDA_HOME/resultados/$i
-mv -r $EXEHDA_HOME/log/* $EXEHDA_HOME/resultados/$i
+mv $EXEHDA_HOME/log/* $EXEHDA_HOME/resultados/$i
 
-#./obtem_resultados.sh ${task};
-
-$i=$i+1
+i=`expr $i + 1` 
 done
 
+echo Executou $times vezes
